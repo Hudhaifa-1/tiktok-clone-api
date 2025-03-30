5340a186814f38e1ec3df97c90a995c869d794e9
Hudhaifa-1 <bitybitiq@gmail.com>
2025-03-30 03:17:13 +0300

    Gloable & post controllers done

---
+++ app/Services/FileService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class FileService{
+    public function updateImage($model, $request){
+
+        // create new manager instance with desired driver
+        $manager = new ImageManager(new Driver());
+
+        // read image from filesystem
+        $image = $manager->read($request->file('image'));
+
+        if(!empty($model->image)){
+            $currentImage = public_path() . $model->image;
+
+            if(file_exists($currentImage) && $currentImage != public_path() . '/user-placeholder.png'){
+                unlink($currentImage);
+            }
+        }
+
+        $file = $request->file('image');
+        $extention = $file->getClientOriginalExtension();
+
+        $image->crop(
+            $request->width,
+            $request->height,
+            $request->left,
+            $request->top
+        );
+
+        $name = time() . '.' . $extention;
+        $image->save(public_path() . '/files/' . $name);
+        $model->image = '/files/' . $name;
+
+        return $model;
+    }
+
+    public function addVideo($model, $request){
+
+        $video = $request->file('video');
+        $extension = $video->getClientOriginalExtension();
+        $name = time() . '.' . $extension;
+        $video->move(public_path() . '/files/' . $name);
+        $model->video = '/files/' . $name;
+
+        return $model;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::post('/update-user-image', [UserController::class,'updateUserImage']);
+
 Route::middleware(['auth:sanctum'])->group( function () {
    Route::get('/logged-in-user', [UserController::class,'loggedInUser']);
 });
